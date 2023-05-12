53ea43c43b55fd2bd0250badb29701e99266afc0
Filipouzo <philiparnal@gmail.com>
2023-05-12 12:32:08 +0200

    initial commit

--- src/rebonjour/settings.py
+++ src/rebonjour/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'rebonjour.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['rebonjour/templates'],
+        'DIRS': ['rebonjour/templates/rebonjour'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
